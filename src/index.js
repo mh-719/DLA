66b020e6a31212f1986371a9c83db418ecdcb76c
mh-719 <86534373+mh-719@users.noreply.github.com>
2022-02-27 02:27:00 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+document.body.style.backgroundColor = '#bdc9e0';
+
+ReactDOM.render(<App />, document.getElementById('root'));
