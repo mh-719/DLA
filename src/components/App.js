0ea1159639262e492d88db7016b6249a2fa61602
mh-719 <86534373+mh-719@users.noreply.github.com>
2022-03-03 02:06:00 -0500

    Add canvas

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import GUI from './GUI';
 import '../styles/App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <main></main>
+      <main>
+        <GUI />
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/components/GUI.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Canvas from './Canvas';
+
+function GUI() {
+  return (
+    <React.Fragment>
+      <Canvas />
+    </React.Fragment>
+  );
+}
+
+export default GUI;
