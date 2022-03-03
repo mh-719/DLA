5f22dd89fe0ab866c8524a4a01b42804c0fb25e5
mh-719 <86534373+mh-719@users.noreply.github.com>
2022-03-03 03:03:00 -0500

    Add buttons for running, pausing, and resetting simulations

--- src/components/GUI.js
+++ src/components/GUI.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from './Title';
 import Canvas from './Canvas';
+import Controls from './Controls';
 
 function GUI() {
   return (
     <React.Fragment>
       <Title />
       <Canvas />
+      <Controls />
     </React.Fragment>
   );
 }
